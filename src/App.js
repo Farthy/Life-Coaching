c0339708f356d468ba295946f2964f9b81de2327
Farthy <senelwafarthy@gmail.com>
2024-06-05 09:53:16 +0300

    Execise section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Exercise from "./components/UI/Exercise";
 import Hero from "./components/UI/Hero";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <Exercise />
     </>
   );
 }
